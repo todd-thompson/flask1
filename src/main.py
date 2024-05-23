7699b92f4dcbd93f0b149ee36303763786459cc2
todd.thompson.mail <todd.thompson.mail@gmail.com>
2024-05-23 21:24:33 +0000

    overwrite index.html in public dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
+from shutil import copy2
 
 from flask import Flask, send_file
+src1 = '/home/user/flask1/src/index.html'
+dest1 = '/home/user/flask1/public/index.html'
+copy2(src1, dest1)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('../public/index.html')
+    return send_file(dest1)
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
